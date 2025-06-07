32c4e77da878bcb17cad9a02297d50927c695316
Tatang Bukhori <tatangb909@gmail.com>
2025-06-07 10:57:33 +0700

    update register

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -25,16 +25,16 @@
 
     <div class="w-2/4 mx-auto">
         <form @submit.prevent="submit">
-            <!-- Text Input Name -->
+
             <TextInput name="name" v-model="form.name" :message="form.errors.name" />
-            <!-- Text Input Email -->
+
             <TextInput name="email" type="email" v-model="form.email" :message="form.errors.email" />
-            <!-- Text Input Password -->
+
             <TextInput name="password" type="password" v-model="form.password" :message="form.errors.password" />
-            <!-- Text Input Confirm Password -->
+
             <TextInput name="confirm password" type="password" v-model="form.password_confirmation" />
             <div>
-                <p class="text-slate-600 mb-2">Already a user? <a href="#" class="text-link">Login</a></p>
+                <p class="text-slate-600 mb-2">Already a user? <a :href="route('login')" class="text-link">Login</a></p>
                 <button class="primary-btn">Register</button>
             </div>
         </form>
